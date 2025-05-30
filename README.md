0edec22d36ae158ab9642c8e88ed26d5eb977d64
Oliver Salzburg <oliver.salzburg@gmail.com>
2025-05-30 20:33:40 +0200

    feat: Use HTML labels

--- README.md
+++ README.md
@@ -1,10 +1,5 @@
-# Project Template
+# OpenTimeline - Working Draft
 
-## Does
-
-- Uses [TypeScript](https://www.typescriptlang.org/)
-- Target application runs in the browser
-- [Vite](https://vitejs.dev/) (uses [esbuild](https://esbuild.github.io/))
-- [EditorConfig](https://editorconfig.org/) + [Biome](https://biomejs.dev/)
-- QA with [GitHub Actions](https://github.com/features/actions)
-- [MkDocs](https://www.mkdocs.org/)-oriented Markdown in mind
+```shell
+dot -Tsvg:cairo timelines/mtg-editions.yml.gv > timelines/mtg-editions.yml.svg
+```
--- source/dot.ts
+++ source/dot.ts
@@ -1,4 +1,5 @@
 import { indent } from "@oliversalzburg/js-utils/data/string.js";
+import { InvalidOperationError } from "@oliversalzburg/js-utils/errors/InvalidOperationError.js";
 
 export type Arrow =
   | "box"
@@ -24,8 +25,8 @@ export type Color = string;
 export type PortPos = "n" | "ne" | "e" | "se" | "s" | "sw" | "w" | "nw" | "c" | "_";
 
 export interface LinkProperties {
-  arrowhead:Arrow;
-  arrowtail:Arrow;
+  arrowhead: Arrow;
+  arrowtail: Arrow;
   color: Color;
   constraint: boolean;
   fillcolor: Color;
@@ -56,12 +57,13 @@ export interface NodeProperties {
 
 const makePropertyString = (properties: Record<string, boolean | number | string>) =>
   Object.entries(properties)
-    .map(([key, _]) => `${key}="${_}"`)
+    .map(([key, _]) => (key === "label" ? `${key}=<${_}>` : `${key}="${_}"`))
     .sort()
     .join("; ");
 
 export const dot = () => {
   const buffer = new Array<string>();
+  const nodeIds = new Map<string, number>();
 
   let indentation = 0;
   let nextNodeIndex = 0;
@@ -78,29 +80,46 @@ export const dot = () => {
     }
   };
 
-  const renderNode = (_: string, options?: Partial<NodeProperties>) =>
-    renderRaw(`"${_}" [${makePropertyString(options ?? { label: _ })};]`);
+  const toHtmlString = (_: string) => `<TABLE ALIGN="CENTER" BORDER="0" CELLBORDER="0" CELLPADDING="0" CELLSPACING="0"><TR><TD ALIGN="TEXT" CELLPADDING="0" CELLSPACING="0">${_.replaceAll(/\n|\\n/g, `<BR ALIGN="CENTER"/>`)}</TD></TR></TABLE>`;
 
-  const renderLink = (a: string, b: string, options?: Partial<LinkProperties>) =>
-    renderRaw(`"${a}" -> "${b}"${options ? ` [${makePropertyString(options ?? {})};]` : ""}`);
+  const renderNode = (_: string, options?: Partial<NodeProperties>) => {
+    if (nodeIds.get(_)) {
+      // Not re-rendering node with already seen title.
+      return;
+    }
+    const id = ++nextNodeIndex;
+    nodeIds.set(_, id);
+    renderRaw(`${id} [${makePropertyString({ label: toHtmlString(_), ...options })};]`);
+  };
+
+  const renderLink = (a: string, b: string, options?: Partial<LinkProperties>) => {
+    const aId = nodeIds.get(a);
+    if (aId === undefined) {
+      throw new InvalidOperationError(`Node with given title is unknown: ${a}`);
+    }
+    const bId = nodeIds.get(b);
+    if (bId === undefined) {
+      throw new InvalidOperationError(`Node with given title is unknown: ${b}`);
+    }
+    renderRaw(`${aId} -> ${bId}${options ? ` [${makePropertyString(options ?? {})};]` : ""}`);
+  };
 
   const renderAnnotation = (_: string, text: string) => {
-    renderNode(`annotation${nextNodeIndex}`, {
-      label: text,
+    const annotationId = nextNodeIndex;
+    renderNode(`annotation${annotationId}`, {
+      label: toHtmlString( text),
       margin: 0.2,
       shape: "plaintext",
       style: "dotted",
     });
 
-    renderLink(`annotation${nextNodeIndex}`, _, {
+    renderLink(`annotation${annotationId}`, _, {
       arrowhead: "none",
       constraint: false,
       penwidth: 0.5,
       style: "dotted",
       tailclip: false,
     });
-
-    ++nextNodeIndex;
   };
 
   return {
