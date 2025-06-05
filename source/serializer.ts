6a7d8b2a298aa4c37f0621ee20587f031042232f
Oliver Salzburg <oliver.salzburg@gmail.com>
2025-06-05 20:31:33 +0200

    fix(serializer): Multi-values not handled

--- source/serializer.ts
+++ source/serializer.ts
@@ -1,11 +1,19 @@
 import { stringify } from "yaml";
-import type { Timeline } from "./types.js";
+import type { Timeline, TimelineDocument, TimelineFlexibleInput } from "./types.js";
 
 export const serialize = (timeline: Timeline, metadata: Record<string | number, unknown> = {}) => {
-  const document = {
+  const document: TimelineDocument = {
     ...timeline.meta,
     ...metadata,
-    timeline: Object.fromEntries(timeline.records),
+    timeline: timeline.records.reduce((serialized, [timestamp, entry]) => {
+      serialized[timestamp] =
+        timestamp in serialized
+          ? Array.isArray(serialized[timestamp])
+            ? [...serialized[timestamp], entry.title]
+            : [serialized[timestamp], entry.title]
+          : entry.title;
+      return serialized;
+    }, {} as TimelineFlexibleInput),
   };
   return stringify(document);
 };
