b7f1bca29bf29888786d8c8bf8611d8bec50eedb
Oliver Salzburg <oliver.salzburg@gmail.com>
2025-08-19 23:30:58 +0200

    fix: svgcat

--- contrib/svgcat.js
+++ contrib/svgcat.js
@@ -34,7 +34,17 @@ const segments = process.argv
 	.filter((_) => !_.startsWith("--"))
 	.sort();
 
-/** @type {Map<string,{width:number;height:number;marker:Array<Record<string,string>>}>} */
+const boundingBox = {
+	top: 0,
+	right: 0,
+	bottom: 0,
+	left: 0,
+};
+// Magic constant pulled from SVG output: y="<this value for exit markers>"
+const MARKER_Y = "-22.67";
+let offsetX = 0;
+let offsetY = 0;
+/** @type {Map<string,{width:number;height:number;marker:Array<Record<string,string>>;x:number}>} */
 const meta = new Map();
 for (const segment of segments) {
 	try {
@@ -53,28 +63,57 @@ for (const segment of segments) {
 			/class="node tx.+?text-anchor="middle" x="(?<x>[^"]+)" y="(?<y>[^"]+)+"/gs,
 		);
 
-		meta.set(segment, {
+		const _m = {
 			width: widthSegment,
 			height: heightSegment,
 			marker: [...txMarker].map((_) => _.groups ?? {}),
-		});
+			x: 0,
+		};
+		meta.set(segment, _m);
+
+		const entryMarker = _m.marker
+			.filter((_) => _.y !== MARKER_Y)
+			.map((_) => Number(_.x))
+			.sort((a, b) => a - b);
+		const exitMarker = _m.marker
+			.filter((_) => _.y === MARKER_Y)
+			.map((_) => Number(_.x))
+			.sort((a, b) => a - b);
+
+		const leftMostEntry = 0 < entryMarker.length ? entryMarker[0] : 0;
+		offsetX -= leftMostEntry;
+
+		_m.x = offsetX;
+
+		boundingBox.right = Math.max(boundingBox.right, offsetX + widthSegment);
+		boundingBox.left = Math.min(boundingBox.left, offsetX);
+		boundingBox.bottom = Math.max(boundingBox.bottom, offsetY + _m.height);
+
+		const leftMostExit = 0 < exitMarker.length ? exitMarker[0] : 0;
+		offsetX += leftMostExit;
+		offsetY += _m.height;
 	} catch (error) {
 		console.error(segment);
 		throw error;
 	}
 }
 
-let offsetX = 0;
-let offsetY = 0;
-const buffer = [];
-const boundingBox = {
-	top: 0,
-	right: 0,
-	bottom: 0,
-	left: 0,
-};
+const leftMostSegment = segments.reduce(
+	(lowest, _) =>
+		(meta.get(_)?.x ?? 0) < lowest ? (meta.get(_)?.x ?? 0) : lowest,
+	0,
+);
+for (const segment of segments) {
+	const segmentMeta = meta.get(segment);
+	if (segmentMeta === undefined) {
+		throw new Error("fuck ts");
+	}
+	segmentMeta.x -= leftMostSegment;
+}
 
-const MARKER_Y = "-22.67";
+const buffer = [];
+offsetY = 0;
+
 for (const segment of segments) {
 	const rawSVG = readFileSync(segment, "utf8");
 	const svgStart = rawSVG.indexOf("<svg");
@@ -86,44 +125,26 @@ for (const segment of segments) {
 		throw new Error("Unexpected miss on segment metadata.");
 	}
 
-	const entryMarker = svgMeta.marker
-		.filter((_) => _.y !== MARKER_Y)
-		.map((_) => Number(_.x))
-		.sort((a, b) => a - b);
-	const exitMarker = svgMeta.marker
-		.filter((_) => _.y === MARKER_Y)
-		.map((_) => Number(_.x))
-		.sort((a, b) => a - b);
-
-	const leftMostEntry = 0 < entryMarker.length ? entryMarker[0] : 0;
-	offsetX -= leftMostEntry;
+	offsetY += svgMeta.height;
+	offsetY -= 3;
+	const translation = `${Math.round(svgMeta.x * 100) / 100} ${Math.round(offsetY * 100) / 100}`;
+	process.stderr.write(`${segment}: ${svgMeta.width} -> ${translation}\n`);
+	offsetY += Number(MARKER_Y);
+	offsetY -= 3;
 
 	let svg = rawSVG.substring(svgStartContent, svgEnd);
-	const svgHeightAdjusted = svgMeta.height + Number(MARKER_Y) - 3;
-	const translation = `${Math.round(offsetX * 100) / 100} ${Math.round((offsetY + svgHeightAdjusted) * 100) / 100}`;
-
-	boundingBox.right = Math.max(boundingBox.right, offsetX + svgMeta.width);
-	boundingBox.left = Math.min(boundingBox.left, offsetX);
-	boundingBox.bottom = Math.max(
-		boundingBox.bottom,
-		offsetY + svgHeightAdjusted,
-	);
-
-	process.stderr.write(`${segment}: ${svgMeta.width} -> ${translation}\n`);
-
 	svg = svg.replace(/translate\(0 [^)]+\)/, `translate(${translation})`);
-
-	const leftMostExit = 0 < exitMarker.length ? exitMarker[0] : 0;
-	offsetX += leftMostExit;
-	offsetY += svgHeightAdjusted;
 
 	buffer.push(svg);
 }
+const width =
+	Math.round(Math.abs(boundingBox.left - boundingBox.right) * 10) / 10;
+const height = Math.round(boundingBox.bottom * 10) / 10;
 buffer.unshift(
 	[
 		`<?xml version="1.0" encoding="UTF-8" standalone="no"?>`,
 		`<!DOCTYPE svg PUBLIC "-//W3C//DTD SVG 1.1//EN" "http://www.w3.org/Graphics/SVG/1.1/DTD/svg11.dtd">`,
-		`<svg width="${boundingBox.right - boundingBox.left}pt" height="${boundingBox.bottom}pt" viewBox="0.00 ${boundingBox.left} ${boundingBox.right - boundingBox.left} ${boundingBox.bottom}" xmlns="http://www.w3.org/2000/svg" xmlns:xlink="http://www.w3.org/1999/xlink">`,
+		`<svg width="${width}pt" height="${height}pt" viewBox="0.00 0.00 ${width} ${height}" xmlns="http://www.w3.org/2000/svg" xmlns:xlink="http://www.w3.org/1999/xlink">`,
 	].join("\n"),
 );
 buffer.push("</svg>");
